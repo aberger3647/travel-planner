b6ff2ec13316a6452dfbb6c592be2abad077f995
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2022-12-12 13:00:45 -0600

    added params to put request

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const { Trip } = require('../../models');
 
+// GET all trips
+router.get('/', async (req, res) => {
+  try{
+      const tripData = await Trip.findAll();
+      res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // CREATE a trip
 router.post('/', async (req, res) => {
   try {
@@ -10,6 +20,17 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// UPDATE a trip
+router.put('/:id', async (req, res) => {
+  try {
+    const tripData = await Trip.update(req.body, {where: { id: req.params.id }});
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 // DELETE a trip
 router.delete('/:id', async (req, res) => {
